dd6fcde82d25ce20897be1da2de8ce4d03fadf90
lanroo <yladacs@gmail.com>
2023-12-18 20:05:54 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    var navLinks = document.getElementById('navLinks');
+    if (navLinks.style.left === '0px') {
+        navLinks.style.left = '-100%';
+    } else {
+        navLinks.style.left = '0px';
+    }
+}
+function toggleMenu() {
+    var navLinks = document.getElementById('navLinks');
+    if (navLinks.style.left === '0px') {
+        navLinks.style.left = '-100%';
+    } else {
+        navLinks.style.left = '0px';
+    }
+}
